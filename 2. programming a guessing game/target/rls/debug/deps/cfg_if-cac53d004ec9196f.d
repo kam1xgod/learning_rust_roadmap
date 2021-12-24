e8252ab588b868a92725a2be9c2699ddd1656c1d
kam1xuseless <80982403+kam1xgod@users.noreply.github.com>
2021-12-24 23:28:51 +0400

    Delete 2. programming a guessing game/target directory

--- 2. programming a guessing game/target/rls/debug/deps/cfg_if-cac53d004ec9196f.d
+++
@@ -1,5 +0,0 @@
-d:\repos\rust book\2. programming a guessing game\guessing_game\target\rls\debug\deps\cfg_if-cac53d004ec9196f.rmeta: C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-d:\repos\rust book\2. programming a guessing game\guessing_game\target\rls\debug\deps\cfg_if-cac53d004ec9196f.d: C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
--- 2. programming a guessing game/target/rls/debug/deps/getrandom-5e163cc3beb34a29.d
+++
@@ -1,9 +0,0 @@
-d:\repos\rust book\2. programming a guessing game\guessing_game\target\rls\debug\deps\getrandom-5e163cc3beb34a29.rmeta: C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs
-
-d:\repos\rust book\2. programming a guessing game\guessing_game\target\rls\debug\deps\getrandom-5e163cc3beb34a29.d: C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs
-
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs:
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs:
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs:
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs:
-C:\Users\kami\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs:
