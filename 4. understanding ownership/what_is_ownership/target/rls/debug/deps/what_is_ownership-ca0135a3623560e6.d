765c6f8827152fce48891321591421e7369a3679
kam1xgog <wtfomgohohoh@gmail.com>
2021-12-31 13:54:17 +0400

    delete target folders.

---
+++ 4. understanding ownership/what_is_ownership/target/rls/debug/deps/what_is_ownership-ca0135a3623560e6.d
@@ -0,0 +1,5 @@
+/home/kami/repos/stupid_boy_learning_rust/4. understanding ownership/what_is_ownership/target/rls/debug/deps/what_is_ownership-ca0135a3623560e6.rmeta: src/main.rs
+
+/home/kami/repos/stupid_boy_learning_rust/4. understanding ownership/what_is_ownership/target/rls/debug/deps/what_is_ownership-ca0135a3623560e6.d: src/main.rs
+
+src/main.rs:
